32f289afea5ca4d6baf3a55a4db4e1f4685d9a13
Samir <skhatri0831@gmail.com>
2021-06-25 01:01:22 -0400

    third commit

--- src/carbonFootPrint/carbonFootPrint.java
+++ src/carbonFootPrint/carbonFootPrint.java
@@ -1,3 +1,4 @@
+// This is carbonfootprint interface only
 package carbonFootPrint;
 
 public interface carbonFootPrint { // this is an interface class. just a skeleton structure that forces other implemented class to call these methods
